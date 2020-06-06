7d9696dc7aa6a85917a41dfaac7c84d62ce2f72e
Samuel Grayson <sam@samgrayson.me>
2020-06-06 15:15:54 -0500

    Fix CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,22 +5,31 @@ cd "$(dirname "${0}")/.."
 package=bollywood_data_science
 src="./${package}"
 check=$([ "${1}" = "check" ] && echo "true" || echo "")
+verbose=${verbose:-}
+
+function now() {
+	python -c 'import datetime; print((datetime.datetime.now() - datetime.datetime(1970, 1, 1)).total_seconds())'
+}
 
 function capture() {
 	local log=$(mktemp)
 	local command=("${@}")
 
+	local command_start="$(now)"
 	"${command[@]}" &> "${log}"
 	local command_exit="${?}"
+	local command_end="$(now)"
+	local command_duration=$(python3 -c "print('{:.2f}'.format(${command_end} - ${command_start}))")
 
-	if [ -s "${log}" -o "${command_exit}" -ne 0 ]; then
+	if [ -s "${log}" -o "${command_exit}" -ne 0 -o -n "${verbose}" ]; then
 		if [ "${command_exit}" -eq 0 ]; then
 			echo -e "\033[32;1m\$ ${command[@]}\033[0m"
 			cat "${log}"
+			echo -e "\033[32;1mexitted ${command_exit} in ${command_duration}\033[0m"
 		else
 			echo -e "\033[31;1m\$ ${command[@]}\033[0m"
 			cat "${log}"
-			echo -e "\033[31;1mexitted ${command_exit}\033[0m"
+			echo -e "\033[31;1mexitted ${command_exit} in ${command_duration}\033[0m"
 			exit "${command_exit}"
 		fi
 	fi
