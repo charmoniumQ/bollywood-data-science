9f189bb253e3152d6165e53e7805f5fc0614acf2
Sam Grayson <sam@samgrayson.me>
2020-06-06 15:28:30 -0500

    Fix ci (#1)
    * I think this fixes CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,28 +5,32 @@ cd "$(dirname "${0}")/.."
 package=bollywood_data_science
 src="./${package}"
 check=$([ "${1}" = "check" ] && echo "true" || echo "")
+verbose=${verbose:-}
+
+function now() {
+	python -c 'import datetime; print((datetime.datetime.now() - datetime.datetime(1970, 1, 1)).total_seconds())'
+}
 
 function capture() {
 	local log=$(mktemp)
 	local command=("${@}")
 
+	local command_start="$(now)"
 	"${command[@]}" &> "${log}"
 	local command_exit="${?}"
+	local command_end="$(now)"
+	local command_duration=$(python3 -c "print('{:.2f}'.format(${command_end} - ${command_start}))")
 
-	if [ -s "${log}" -o "${command_exit}" -ne 0 ]; then
+	if [ -s "${log}" -o "${command_exit}" -ne 0 -o -n "${verbose}" ]; then
 		if [ "${command_exit}" -eq 0 ]; then
 			echo -e "\033[32;1m\$ ${command[@]}\033[0m"
 			cat "${log}"
+			echo -e "\033[32;1mexitted ${command_exit} in ${command_duration}s\033[0m"
 		else
 			echo -e "\033[31;1m\$ ${command[@]}\033[0m"
 			cat "${log}"
-			echo -e "\033[31;1mexitted ${command_exit}\033[0m"
-			if [ -z "${check}" ]; then
-				# We are not in check mode
-				# I am running interactively
-				# Exit on first failure, because fixing early failures could fix future ones
-				exit "${command_exit}"
-			fi
+			echo -e "\033[31;1mexitted ${command_exit} in ${command_duration}s\033[0m"
+			exit "${command_exit}"
 		fi
 	fi
 }
@@ -67,4 +71,4 @@ capture \
 
 capture \
 	poetry run \
-		pytest --quiet --cov="${src}" --cov=tests --cov-report=term-missing $([ -n "${check}" ] || echo "--exitfirst")
+		pytest --quiet --cov="${src}" --cov=tests --cov-report=term-missing --exitfirst
