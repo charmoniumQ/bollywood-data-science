8fdcaa96514fb03338b0f96bffe7ba6720e2fff3
Samuel Grayson <sam@samgrayson.me>
2020-06-21 19:26:25 +0000

    Fix SPARQL

---
+++ bollywood_data_science/main.py
@@ -0,0 +1,25 @@
+import yaml
+from .sparql_graph import cached_sparql_graph
+
+def main() -> None:
+    with open("res/config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    query_name = "wikidata_get"
+
+    endpoint = config["queries"][query_name]["endpoint"]
+    query = config["queries"][query_name]["query"]
+    minify = config["queries"][query_name].get("minify", False)
+    return_format = config["queries"][query_name]["return_format"]
+    headers = config["queries"][query_name]["headers"]
+    method = config["queries"][query_name]["method"]
+    graph = cached_sparql_graph(
+        endpoint, query, return_format=return_format, headers=headers, method=method,
+    )
+    people = set()
+    for subj, verb, obj in graph:
+        people.add(subj)
+    print(dict(triples=len(graph), people=len(people)))
+
+if __name__ == '__main__':
+    main()
---
+++ bollywood_data_science/sparql_graph.py
@@ -0,0 +1,69 @@
+import re
+import sys
+import urllib.parse
+from typing import Any, Dict, Optional, cast, Callable
+
+import charmonium.cache as ch_cache
+import charmonium.time_block as ch_time_block
+import rdflib
+import requests
+
+
+def sparql_graph(
+    endpoint: str,
+    query: str,
+    return_format: str = "XML",
+    headers: Optional[Dict[str, str]] = None,
+    method: str = "POST",
+    params: Optional[Dict[str, str]] = None,
+    minify: bool = False,
+    **kwargs: Any,
+) -> rdflib.Graph:
+    """Query a SPARQL endpoint, but cache the resulting RDF"""
+
+    if minify:
+        query = re.sub(r"\s+", " ", query.strip())
+
+    headers_ = headers if headers else dict()
+    params_ = params if params else dict()
+
+    # This ensures sure I never use params where I meant params_
+    del headers, params
+
+    if method == "POST":
+        http_rsp = requests.post(
+            url=endpoint,
+            headers=headers_,
+            data="query=" + urllib.parse.quote(query),
+            **kwargs,
+        )
+    elif method == "GET":
+        http_rsp = requests.get(
+            url=endpoint, headers=headers_, params={"query": query, **params_}, **kwargs
+        )
+    else:
+        raise RuntimeError(f"Method {method} is not supported (GET, POST)")
+
+    try:
+        http_rsp.raise_for_status()
+    except requests.HTTPError:
+        print(http_rsp.text, file=sys.stderr)
+
+    text_req = http_rsp.text
+
+    if rdflib.plugin.get(return_format, rdflib.parser.Parser):
+        graph = rdflib.Graph()
+        graph.parse(data=text_req, format=return_format)
+        return graph
+    else:
+        raise RuntimeError(f"rdflib has no plugin for {return_format}")
+
+
+cached_sparql_graph = cast(
+    Callable[..., rdflib.Graph],
+    ch_time_block.decor()(
+    ch_cache.decor(
+        ch_cache.DirectoryStore.create("tmp"), verbose=True, name="cache_sparql_graph",
+    )(sparql_graph)
+    )
+)
