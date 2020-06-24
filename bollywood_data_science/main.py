7a631bb4667641476ecaf14b01f5e8f8e4dadb98
Samuel Grayson <sam@samgrayson.me>
2020-06-24 12:51:23 -0500

    Fix

--- bollywood_data_science/main.py
+++ bollywood_data_science/main.py
@@ -1,9 +1,19 @@
+from typing import Set
 import charmonium.time_block as ch_time_block
 import yaml
 import sys
 
 from .imdb_graph import imdb_graph
 from .sparql_graph import cached_sparql_graph
+
+
+def is_int(string: str):
+    try:
+        int(string)
+    except ValueError:
+        return False
+    else:
+        return True
 
 
 def main() -> None:
@@ -21,14 +31,18 @@ def main() -> None:
                 "SELECT DISTINCT ?person WHERE { ?person ?prop ?thing }"
             )
         }
-        imdb_ids = {
-            str(row[0])
-            for row in graph.query(
-                "SELECT DISTINCT ?personImdbId WHERE { ?person wdt:P345 ?personImdbId }"
-            )
-        }
+        imdb_ids: Set[str] = set()
+        for person, imdb_id_ in graph.query(
+                "SELECT DISTINCT ?person ?personImdbId WHERE { ?person wdt:P345 ?personImdbId }"
+        ):
+            imdb_id = str(imdb_id_)
+            if not is_int(imdb_id[2:]):
+                print(f"Bad IMDDb id ({imdb_id}) for {person}")
+            else:
+                imdb_ids.add(imdb_id)
+        imdb_ids = sorted(imdb_ids)
+
     print(dict(triples=len(graph), people=len(people), imdb_ids=len(imdb_ids),))
-    imdb_ids = sorted(imdb_ids)
     start = int(sys.argv[1])
     stop  = int(sys.argv[2])
     graph_imdb = imdb_graph(imdb_ids[start:stop], db=False)
